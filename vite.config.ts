01b18da6c21d4c70d92a6ad31ef5aeea6fb35478
Umer Farooq <fumer8353@gmail.com>
2026-06-23 21:54:24 +0500

    4th commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5174,
+    host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://bookstore-backend-g0f7bzbvargjedc7.australiaeast-01.azurewebsites.net",
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
